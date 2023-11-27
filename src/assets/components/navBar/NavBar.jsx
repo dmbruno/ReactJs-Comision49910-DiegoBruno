c3d3530849aacc93cf53e82e74c0a8bd26ce5348
diego <dmbruno61@gmail.com>
2023-11-27 11:02:50 -0300

    1era pre-entrega-React-DiegoBruno

--- src/assets/components/navBar/NavBar.jsx
+++ src/assets/components/navBar/NavBar.jsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
                 </div>
                 <ul className="flex gap-11">
                     <li className="text-white text-lg hover:text-black font-bold font-mono">Login</li>
-                    <li className="text-white text-lg hover:text-black font-bold font-mono">Menu del OSO</li>
+                    <li className="text-white text-lg hover:text-black font-bold font-mono">Menu del Oso</li>
                     <li className="text-white text-lg hover:text-black font-bold font-mono">Pedir Cuenta</li>
                 </ul>
                 <div className="contenedor-icono">
