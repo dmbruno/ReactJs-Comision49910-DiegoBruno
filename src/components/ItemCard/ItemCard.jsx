29be4b081d9874a91513684af829ddc0b276430c
diego <dmbruno61@gmail.com>
2023-12-14 13:56:47 -0300

    fix router 2

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -7,25 +7,27 @@ import './ItemCard.css'
 
 
 
-export const ItemCard = ({item}) => {
+export const ItemCard = ({ item }) => {
     return (
-        <div className="contenedor-card rounded bg-yellow-700 h-auto" key={item.id}>
-            <article className="container-article flex flex-col items-center">
-                <div className="portada flex-1">
-                    <img className="img rounded" src={item.img} alt="foto de plato" />
-                </div>
-                <div className="contenedor-info flex-1 relative">
-                    <h3 className="font-extrabold text-center mt-9 font-serif text-3xl">{item.name}</h3>
-                    <div className="container-texto">
-                        <hr className="m-10" />
-                        <p className="text-gray-300 font-bold font-mono text-center mt-3">Que tiene? : {item.description}</p>
+        
+            <div className="contenedor-card rounded bg-yellow-700 h-auto">
+                <article className="container-article flex flex-col items-center">
+                    <div className="portada flex-1">
+                        <img className="img rounded" src={item.img} alt="foto de plato" />
                     </div>
-                </div>
-                <button className="bg-yellow-200 rounded py-2 px-4 font-mono text-black mt-10">
-                    <Link to={`/item/${item.id}`}>Ver Mas</Link>
-                </button>
-            </article>
-        </div>
+                    <div className="contenedor-info flex-1 relative">
+                        <h3 className="font-extrabold text-center mt-9 font-serif text-3xl">{item.name}</h3>
+                        <div className="container-texto">
+                            <hr className="m-10" />
+                            <p className="text-gray-300 font-bold font-mono text-center mt-3">Que tiene? : {item.description}</p>
+                        </div>
+                    </div>
+                    <button className="bg-yellow-200 rounded py-2 px-16 font-mono text-black mt-10 shadow-black">
+                        <Link to={`/item/${item.id}`}>Ver Mas</Link>
+                    </button>
+                </article>
+            </div>
+        
     )
 }
 
---
+++ src/components/botones/Boton.jsx
@@ -0,0 +1,10 @@
+
+
+export const Boton = ({texto}) => {
+    return (
+
+        <button className="flex justify-center text-white rounded bg-yellow-900 font-mono hover:bg-slate-600 py-3 px-6">
+                {texto}
+        </button>
+    )
+}
