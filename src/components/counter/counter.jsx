1a12d2c1f577577c6df071ff0332792c4a0bcf90
diego <dmbruno61@gmail.com>
2023-12-20 21:56:59 -0300

    fix botones

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -21,11 +21,11 @@ export const Counter = () => {
     return (
         <>
             <div className="flex flex-row mt-4 px-5">
-                <Boton texto="+" onClick={sumar}></Boton>
+                <Boton texto="-" onClick={restar}></Boton>
                 <div>
                     <p className="parrafo flex justify-center py-1 px-3"> Cantidad: {counter} </p>
                 </div>
-                <Boton texto="-" onClick={restar}></Boton>
+                <Boton texto="+" onClick={sumar}></Boton>
             </div>
             <div className="mt-4 px-5">
                 <Boton texto={"Agregar al carrito"}></Boton>
