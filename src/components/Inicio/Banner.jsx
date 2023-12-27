9cf91e1f29be200d6c3f25091e831554758a57e0
diego <dmbruno61@gmail.com>
2023-12-27 15:10:39 -0300

    estilos tailwind

--- src/components/Inicio/Banner.jsx
+++ src/components/Inicio/Banner.jsx
@@ -4,13 +4,9 @@
 
 export const Banner = () => {
     return (
-        <div className="bg-red-700 mb-3 w-2/3 sm:w-2/3 border-2 border-indigo-600">
-            <div className="w-100 bg-black sm:h-1/2">
-                <img className="" src="./banner.jpg" alt="foto banner" />
-            </div>
-            <div className="hidden sm:block font-bold text-lg">
-                <h2> texto </h2>
-            </div>
+        <div className="bg-red-700 mb-3 w-2/3  border-2 border-indigo-600 relative">
+                <img className="h-full object-cover" src="./banner.jpg" alt="foto banner" />
+                <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lime-200 font-extrabold text-[50px]">GRIZZLY</h2>
         </div>
     )
 }
