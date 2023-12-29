0e6c22e353d97e4340e84661ae1806f2a0dbf787
diego <dmbruno61@gmail.com>
2023-12-29 15:09:09 -0300

    fix estilos tailwind

--- src/components/itemDetail/ItemDetail.jsx
+++ src/components/itemDetail/ItemDetail.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import QuantitySelector from "./QuantitySelector";
 import { CartContext } from "../../Context/CartContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
@@ -26,7 +26,7 @@ const ItemDetail = ({ item }) => {
 
     return (
         <>
-            <div className="flex flex-col items-center bg-white shadow-md rounded-lg p-4 ">
+            <div className="flex flex-col items-center bg-white shadow-md rounded-lg p-4">
                 <button className="mb-4 py-1 px-4 border-dashed border-2 border-indigo-600 rounded-lg hover:bg-yellow-700" onClick={handleAtras}>Atras</button>
                 <h3 className="font-semibold text-lg mb-2">{item.name}</h3>
                 <hr className="border-gray-200 w-full mb-4" />
@@ -39,9 +39,9 @@ const ItemDetail = ({ item }) => {
                         </div>
                         {
                             isInCart(item.id)
-                                ? <button className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded mb-2">
+                                ? <Link to="/cart" className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded mb-2">
                                     Terminar mi compra
-                                </button>
+                                </Link>
                                 : <>
                                     <QuantitySelector
                                         cantidad={cantidad}
