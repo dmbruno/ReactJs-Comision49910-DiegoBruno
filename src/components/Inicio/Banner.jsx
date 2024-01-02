5275d286a6fc17b42e36839f87ed38900e057c6f
diego <dmbruno61@gmail.com>
2024-01-02 12:15:14 -0300

    fix responsive

--- src/components/Inicio/Banner.jsx
+++ src/components/Inicio/Banner.jsx
@@ -6,7 +6,7 @@ export const Banner = () => {
     return (
         <div className="mb-3 w-2/3 relative">
                 <img className="h-full object-cover rounded-lg drop-shadow-2xl" src="./banner.jpg" alt="foto banner" />
-                <h2 className="absolute font-bold top-[100px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lime-200 sm:font-extrabold text-[50px] sm:tracking-[25px] sm:pl-[15px]">GRIZZLY</h2>
+                <h2 className="absolute font-bold top-[100px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lime-200 sm:font-extrabold text-[50px] sm:tracking-[25px] sm:pl-[15px] lg:text-[70px]">GRIZZLY</h2>
         </div>
     )
 }
