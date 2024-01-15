06e8fb3f4b2d67bb73b642a27e516625e6e604d2
diego <dmbruno61@gmail.com>
2024-01-15 15:47:42 -0300

    estilos2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,8 +7,8 @@ export const Footer = () => {
     const { logout, user } = useContext(UserContext)
 
     return (
-        <div className="m-auto flex felx-col justify-center h-40 items-center border-t-2">
-            <div className="flex flex-col m-7 gap-2">
+        <div className="m-auto flex felx-col justify-center h-40 border-t-2 min-h-screen">
+            <div className="mb-auto flex flex-col m-7 gap-2">
                 {
                     user.logged && <>
                         <h2 className="font-bold text-[12px]">{user.email}</h2>
@@ -16,7 +16,7 @@ export const Footer = () => {
                     </>
                 }
             </div>
-            <div className="flex flex-row">
+            <div className="flex flex-row mb-auto">
                 <h3 className="text-black font-bold flex items-center hover:text-yellow-800 m-4 cursor-pointer sm:text-2xl">Quienes Somos</h3>
                 <h3 className="text-black font-bold flex items-center hover:text-yellow-800 m-4 cursor-pointer sm:text-2xl">Contactanos</h3>
             </div>
